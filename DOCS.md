e0b05dcdd00e8288fdc921f5c779f73e5bfae5f8
Anjanique <anjaniquemackey@gmail.com>
2023-02-08 20:30:27 -0800

    adds view comment sec to post-card

--- DOCS.md
+++ DOCS.md
@@ -13,6 +13,62 @@ Challenging things I've done:
 
 - Tried different solutions based on my goal. I tried bootstrap-react at first because my goal was fast styling. I tried and failed to get Bootstrap to completely cooperate, and could forsee problems developing with it given its limited documentation, so I switched over to Tailwind, and did not regret.
 - Figured out how to customize Tailwind, and played around with designing responsively with my customized class names.
+- I wanted to implement a comment input that would resize as the person typed so thta there would be no scroll overflow, so I tried different things such as using an input instead, looking at how other websites would've implememted it, etc. I eventually came upon a stackoverflow question that was using jquery to do it, but i found an answer that showed a way to do it with react and was able to implement and troubleshoot it myself.
+Heres the link: https://stackoverflow.com/a/68638979/18193299
+My implementaion is in /components/Post.jsx
+Heres the code:
+```
+const {
+  useLayoutEffect,
+  useState,
+  useRef
+} = React;
+
+const TextArea = () => {
+  const ref = useRef();
+  const [value, setValue] = useState('Some initial text that both wraps and uses\nnew\nlines');
+
+  // This only tracks the auto-sized height so we can tell if the user has manually resized
+  const autoHeight = useRef();
+
+  useLayoutEffect(() => {
+    if (!ref.current) {
+      return;
+    }
+
+    if (
+      autoHeight.current !== undefined &&
+      ref.current.style.height !== autoHeight.current
+    ) {
+      // don't auto size if the user has manually changed the height
+      return;
+    }
+
+    ref.current.style.height = "auto";
+    ref.current.style.overflow = "hidden";
+    const next = `${ref.current.scrollHeight}px`;
+    ref.current.style.height = next;
+    autoHeight.current = next;
+    ref.current.style.overflow = "auto";
+  }, [value, ref, autoHeight]);
+
+
+  return (
+    <textarea
+      ref={ref}
+      style={{
+        resize: 'vertical',
+        minHeight: '1em',
+      }}
+      value={value}
+      onChange={event => setValue(event.target.value)}
+    />
+  );
+}
+
+ReactDOM.render(<TextArea />, document.getElementById('app'))
+```
+
 
 
 TOFIX:
--- client/src/components/Post.jsx
+++ client/src/components/Post.jsx
@@ -1,77 +1,47 @@
 import { FiThumbsDown, FiThumbsUp } from "react-icons/fi";
-import {BiSend} from 'react-icons/bi'
-import { useState } from "react";
+import { BiSend } from 'react-icons/bi'
+import {AiOutlineComment} from 'react-icons/ai'
+import { useState, useRef, useLayoutEffect } from "react";
 
+    // TODO add a function that handles dates that shows how long ago the post was posted
 export default function Post(data) {
     const { title, description, datePosted, userName } = data;
+    const ref = useRef();
 
     const [btnEffect, setBtnEffect] = useState(false)
 
-    // TODO add a function that handles dates that shows how long ago the post was posted
+    // used for textarea auto-resizing
+    const [commentValue, setCommentValue] = useState('');
+
+     // This only tracks the auto-sized height so we can tell if the user has manually resized
+    const autoHeight = useRef();
+
+    useLayoutEffect(() => {
+    if (!ref.current) {
+      return;
+    }
+    if (
+      autoHeight.current !== undefined &&
+      ref.current.style.height !== autoHeight.current
+    ) {
+      // don't auto size if the user has manually changed the height
+      return;
+    }
+    ref.current.style.height = "30px";
+    ref.current.style.overflow = "hidden";
+    const next = `${ref.current.scrollHeight}px`;
+    ref.current.style.height = next;
+    autoHeight.current = next;
+    ref.current.style.overflow = "auto";
+  }, [commentValue, ref, autoHeight]);
 
     return (
-        // <div className="
-        // w-full
-        // p-4
-        // pb-2
-        // text-my-cream
-        // bg-my-light-blue
-        // "
-        // >   
-
-        //     <h2>{title}</h2>
-        //     <span className=" max-md:hidden">@{userName}</span>
-        //     <p className="break-all">{description}</p>
-
-        //     {/* is hidden for larger screens */}
-        //     <div className="flex flex-row justify-between items-center gap-4 mt-2 md:hidden">
-        //         <div className="flex flex-row justify-center items-center gap-2">
-        //             <img
-        //                 src="https://api.multiavatar.com/Starcrasher.png" alt="avater"
-        //                 className="w-10"
-        //             />
-        //             <span> @{userName}</span>
-        //         </div>
-        //         <span>{datePosted}</span>
-        //     </div>
-
-        //     {/* CARD FOOTER */}
-        //     <div className="
-        //     border-t-2
-        //     w-full
-        //     mt-4
-        //     p-2
-        //     flex flex-row justify-center items-center
-        //     md:justify-between"
-        //     >
-        //         <div className="flex flex-row justify-evenly items-center gap-4 ">
-        //             <div className="hover:text-red flex flex-row justify-center items-center gap-2">
-        //                 <span>Disagree</span>
-        //                 <FiThumbsDown />
-        //             </div>
-        //             <div className="hover:text-green flex flex-row justify-center items-center gap-2">
-        //                 <FiThumbsUp />
-        //                 <span>Agree</span>
-        //             </div>
-                    
-        //         </div>
-                
-        //         {/* is hidden for smaller screens */}
-        //         <div className="flex flex-row justify-end items-center gap-4 max-md:hidden">
-        //             <span className="flex flex-col">@{userName} </span>
-        //             <img
-        //                 src="https://api.multiavatar.com/Starcrasher.png" alt="avater"
-        //                 className="w-10"
-        //             />
-        //         </div>
-        //     </div>
-
-        // </div>
         <div className="
         w-full
         p-4
         text-my-cream
         bg-my-light-blue
+        rounded-md
         ">
             {/* card header */}
             <div className="flex flex-row justify-start gap-2">
@@ -115,12 +85,20 @@ export default function Post(data) {
 
             {/* comment input */}
             <div className="flex flex-row justify-evenly items-center gap-2">
-                <textarea type="text" name="comment" id="comment" placeholder="Comment" className="
+                <textarea type="text"
+                name="comment"
+                id="comment"
+                placeholder="Comment"
+                ref={ref}
+                className="
                 w-full
                 rounded-lg
                 text-my-dark-blue
-                h-8
-                " />
+                p-2
+                "
+                value={commentValue}
+                onChange={event => setCommentValue(event.target.value)} />
+                
                 <button className={`
                 ${btnEffect && "animate-wiggle"}
                     p-1
@@ -135,6 +113,15 @@ export default function Post(data) {
                     onAnimationEnd={() => setBtnEffect(false)}>
                     <BiSend/>
                 </button>
+            </div>
+
+            {/* view more comments */}
+            <div className="
+            p-2
+            cursor-pointer
+            grid place-items-center grid-flow-row">
+                <AiOutlineComment className="text-3xl" />
+                <span>view comments</span>
             </div>
         </div>
     )
