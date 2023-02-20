1cda1a7aa85655018e3a9d2aa3f70e0c4e739b40
Anjanique <anjaniquemackey@gmail.com>
2023-02-19 19:47:03 -0800

    adds responsiveness and new favicon

--- client/src/pages/Auth.jsx
+++ client/src/pages/Auth.jsx
@@ -28,24 +28,25 @@ export default function Auth() {
             {/* page */}
             <div className="
             text-my-dark-blue
-            flex flex-col justify-center
+            flex flex-col justify-start items-center
             pt-4
             min-h-[91vh]
-            md:flex-row md:items-center
+            md:flex-row md:justify-center
             ">
                 <div className="
                 flex flex-col justify-evenly items-center gap-8
-                w-full"
+                w-full
+                mb-8 md:m-0"
                 >
                     <div className="max-md:text-center">
                         <span className="">Got an opinion? Share it here.</span>
 
-                        <h1 className="text-4xl">
-                            {isUserCreatingAcc ? "Create an account." : "Log in."}
+                        <h1 className="text-2xl md:text-4xl">
+                            {isUserCreatingAcc ? "Create an account." : "Sign in."}
                         </h1>
                     </div>
 
-                    <img src={Img} alt="girl voting" className="w-min h-0 md:h-[300px]" />
+                    <img src={Img} alt="girl voting" className="w-min h-28 md:h-[300px]" />
                 </div>
 
                 <AuthForm isUserCreatingAcc={isUserCreatingAcc} setIsUserCreatingAcc={setIsUserCreatingAcc} />
